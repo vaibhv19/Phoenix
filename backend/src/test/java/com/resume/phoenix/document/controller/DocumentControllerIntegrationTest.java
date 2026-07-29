b1bba9f62aa701f757b09922dd281bbba46546a0
Your Name <vaibhavkrgupta369@gmail.com>
2026-07-29 20:34:56 +0530

    Update DocumentControllerIntegrationTest.java

--- backend/src/test/java/com/resume/phoenix/document/controller/DocumentControllerIntegrationTest.java
+++ backend/src/test/java/com/resume/phoenix/document/controller/DocumentControllerIntegrationTest.java
@@ -37,7 +37,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 @AutoConfigureMockMvc
